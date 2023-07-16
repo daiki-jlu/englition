92b9cfb356462379d2eadb14f65cc88675cf01ac
M0c48n <d41ki103@gmail.com>
2023-07-17 00:43:08 +0900

    翻訳ボタン、Notionボタンの仮実装

---
+++ src/components/Notion/NotionButton.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NotionButton() {
+    return <button>Save to Notion</button>;
+}
+
+export default NotionButton;
---
+++ src/components/Translation/TranslationButton.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TranslationButton() {
+    return <button>Translate</button>;
+}
+
+export default TranslationButton;
