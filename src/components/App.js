afff4e9422f023f3cbf4c649b04d04229e46a831
M0c48n <d41ki103@gmail.com>
2023-07-17 00:38:28 +0900

    Appコンポーネント実装

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import TranslationButton from './Translation/TranslationButton';
+import NotionButton from './Notion/NotionButton';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Englition</h1>
+      <TranslationButton />
+      <NotionButton />
     </div>
   );
 }
