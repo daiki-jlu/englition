c4005485075950f6ae75c64e212b61376d9b15a2
M0c48n <d41ki103@gmail.com>
2023-07-18 23:46:27 +0900

    フォーム作成

---
+++ src/components/Notion/NotionForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NotionForm() {
+    return (
+        <form>
+            <input type="text" placeholder="Enter text to save to Notion" />
+            <button>Save</button>
+        </form>
+    );
+}
+
+export default NotionForm;
---
+++ src/components/Translation/TranslationForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TranslationForm() {
+    return (
+        <form>
+            <input type="text" placeholder="Enter text to translate" />
+            <button>Translate</button>
+        </form>
+    );
+}
+
+export default TranslationForm;
