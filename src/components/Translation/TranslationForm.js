274fec1d1fe5e332507600af5709c1ea16db42bd
M0c48n <d41ki103@gmail.com>
2023-07-20 00:22:09 +0900

    翻訳フォーム実装

--- src/components/Translation/TranslationForm.js
+++ src/components/Translation/TranslationForm.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { translateText } from "../../services/translate";
 
 function TranslationForm() {
+    const [text, setText] = useState("");
+    const [translation, setTranslation] = useState("");
+
+    const handleTranslate = async (event) => {
+        event.preventDefault();
+        try {
+            const translatedText = await translateText(text);
+            setTranslation(translatedText);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
-        <form>
-            <input type="text" placeholder="Enter text to translate" />
+        <form onSubmit={handleTranslate}>
+            <input
+                type="text"
+                placeholder="Enter text to translate"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+            />
             <button>Translate</button>
+            {translation && <p>{translation}</p>}
         </form>
     );
 }
