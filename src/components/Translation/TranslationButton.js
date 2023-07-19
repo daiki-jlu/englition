33932aa7750b8f46f5d23ec01439a60899c17a21
M0c48n <d41ki103@gmail.com>
2023-07-19 23:54:27 +0900

    翻譯ボタン実装

--- src/components/Translation/TranslationButton.js
+++ src/components/Translation/TranslationButton.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import TranslationForm from "./TranslationForm";
 
 function TranslationButton() {
-    return <button>Translate</button>;
+    const [showForm, setShowForm] = useState(false);
+
+    const handleButtonClick = () => {
+        setShowForm(!showForm);
+    };
+
+    return (
+        <div>
+            <button onClick={handleButtonClick}>Translate</button>
+            {showForm && <TranslationForm />}
+        </div>
+    );
 }
 
 export default TranslationButton;
